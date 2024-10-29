1c25f9d6ca8eb57ba27c1e7d9a629f24d3400f8c
soul071213 <js071213asd@gmail.com>
2024-10-29 20:16:59 +0900

    chapter05 컴포넌트,props

---
+++ src/chapter05/Comment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comment(props){
+    return(
+        <div>
+            <h1>대왕 카스테라</h1>
+        </div>
+    );
+}
+
+export default Comment;
---
+++ src/chapter05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+function CommnetList(props){
+    return (
+        <div>
+            <Comment />
+        </div>
+    );
+}
+
+export default CommnetList;
